b4e44821fb29d47085b210fa295560a65e25973d
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-02-24 23:28:21 -0500

    Caso 2: solo un elemento (ROJO)

--- tests/test_conjunto.py
+++ tests/test_conjunto.py
@@ -5,3 +5,6 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
+    def test_conjunto_un_elemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual( conjunto.promedio(),5)
