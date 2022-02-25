b4416e8afa460cd1e0b85d0fb09b258da6763f57
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-02-24 23:30:07 -0500

    Caso 2: Solo 1 elemento (VERDE)

--- KataTDD/conjunto.py
+++ KataTDD/conjunto.py
@@ -4,4 +4,7 @@ class Conjunto:
         self.__conjunto = conjunto
     
     def promedio(self):
-        return None
+        if(len(self.__conjunto) == 1):
+            return (self.__conjunto[0])
+        else:
+            return None
